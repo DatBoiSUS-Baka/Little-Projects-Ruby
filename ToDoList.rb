6e76ea73ad844ac41358e65946babd77b50995b6
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-06-22 20:29:20 +0700

    Initial creation of the ToDoList.rb file

---
+++ ToDoList.rb
@@ -0,0 +1,8 @@
+# ======================================================
+#   Filename     : ToDoList.rb
+#   Programmer   : Kasyful Haq Bachariputra
+#   Date         : 22 Juni 2025
+#   Email        : kasyfulhaqb@upi.edu
+#   Description  : A simple command line program that
+#                  is able to make a todo list into a txt file
+# ======================================================
