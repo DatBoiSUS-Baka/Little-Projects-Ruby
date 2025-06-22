e095425e0329e95b36bc400dc60e624ffb05481a
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-06-22 20:44:58 +0700

    Created read_file method for reading file in ToDoList.rb and creating List.txt

--- ToDoList.rb
+++ ToDoList.rb
@@ -6,3 +6,15 @@
 #   Description  : A simple command line program that
 #                  is able to make a todo list into a txt file
 # ======================================================
+
+def read_file()
+  file = File.open('List.txt', 'r')
+
+  file.readlines.each.with_index do |line, counter|
+    puts (counter + 1).to_s + ": " + line
+  end
+
+  file.close
+end
+
+read_file()
